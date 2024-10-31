290a425942559c6a8a7346ed601175b8232aa016
Martin0526 <maring0526@skola.goteborg.se>
2024-10-31 15:59:23 +0100

    torsdag lektion

---
+++ Assets/Scripts/MaterialSwapper/objectSwapper.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class objectSwapper : MonoBehaviour
+{
+
+
+    public Mesh newMesh;
+
+
+    bool isNewMeshActive = false;
+    Mesh oldMesh;
+
+
+    void Start()
+    {
+        oldMesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    public void SwapMesh()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+
+        isNewMeshActive = !isNewMeshActive;
+
+        if (isNewMeshActive)
+            meshFilter.mesh = newMesh;
+        else
+            meshFilter.mesh = oldMesh;
+    }
+
+
+
+}
---
+++ Assets/Scripts/sounds/sound_Player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class sound_Player : MonoBehaviour
+{
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+
+
+    void Update()
+    {
+        
+    }
+}
